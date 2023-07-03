998992952e1061769c57649fa241d743ee75e50e
AXS05000 <axssxs00@gmail.com>
2023-07-02 21:42:46 -0300

    V - 02-07-2023 - 21:42

--- projeto_pdf/celery.py
+++ projeto_pdf/celery.py
@@ -24,7 +24,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'add-every-5-minutes': {
         'task': 'notas.tasks.task_atualizar_notas',
-        'schedule': crontab(minute='*/5'),
+        'schedule': crontab(minute=0),
     },
     # Aqui você pode adicionar mais tarefas periódicas conforme necessário Parar se atualizar de 5 minutos:  ######'schedule': crontab(minute='*/5'),###### 'schedule': crontab(minute=0),
 }
