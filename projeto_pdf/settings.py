2afe275bb7096b4c38d797b624cb808d23767876
AXS05000 <axssxs00@gmail.com>
2023-09-25 15:47:03 -0300

    V - 25-09-2023 - 15:41

--- projeto_pdf/settings.py
+++ projeto_pdf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vlg4fjvkt7y*&)q@0bnhfe!tygcq#t31lem3uu$m3f5w95=pt7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['go2b-bi.com.br', 'www.go2b-bi.com.br']
 
