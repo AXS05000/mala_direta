633cc4782d0270ec420447b5e2bc4aa902fa3933
AXS05000 <axssxs00@gmail.com>
2023-10-02 18:24:13 -0300

    V - 02-10-2023 - 18:24

--- notas/templatetags/notas_filters.py
+++ notas/templatetags/notas_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def format_cnpj(value):
     return f"{value[:2]}.{value[2:5]}.{value[5:8]}/{value[8:12]}-{value[12:14]}"
+
+@register.filter(name='replace_pipe')
+def replace_pipe(value):
+    return value.replace('|', '<br>')
